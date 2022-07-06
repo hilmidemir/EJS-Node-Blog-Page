79e2f306ffc10560b28ced3d316f0903262360b9
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-07-06 19:18:15 +0300

    a

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,7 +15,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", {startingContent:homeStartingContent});
+  res.render("home", {startingContent:homeStartingContent, posts:posts});
+});
+
+app.post("/", function(req, res) {
+
 });
 
 app.get("/about", function(req, res) {
@@ -34,6 +39,8 @@ app.post("/compose", function(req, res) {
     title: req.body.postTitle,
     content: req.body.postBody
   }
+  posts.push(post);
+  res.redirect("/");
 });
 
 
