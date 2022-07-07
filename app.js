30f366a1d5671344c7c34ca8ca1c2a9ee28d7f82
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-07-07 14:32:24 +0300

    posts

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const posts = [];
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -15,7 +15,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home", {startingContent:homeStartingContent, posts:posts});
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts});
 });
 
 app.post("/", function(req, res) {
@@ -23,11 +25,11 @@ app.post("/", function(req, res) {
 });
 
 app.get("/about", function(req, res) {
-  res.render("about", {aboutContent:aboutContent});
+  res.render("about", {aboutContent: aboutContent});
 });
 
 app.get("/contact", function(req, res) {
-  res.render("contact", {contactContent:contactContent});
+  res.render("contact", {contactContent: contactContent});
 });
 
 app.get("/compose", function(req, res) {
