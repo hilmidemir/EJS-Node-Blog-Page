98673ad735b7117da95cc97851ce69eff8e42ece
Hilmi Demir <hilmidemirbjk@gmail.com>
2022-07-08 17:55:27 +0300

    compleate posts pages

--- app.js
+++ app.js
@@ -49,9 +49,10 @@ app.get("/posts/:postName", function(req, res) {
     const storedTitle = _.lowerCase(post.title);
 
     if(storedTitle === requestedTitle){
-      console.log("Match Found!!!");
-    }else{
-      console.log("Match cannot find!");
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     }
   });
 
